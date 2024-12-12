9355218f6a1fd49241280348e02f1b87f0668908
Achyuth Suchit <achyuthsuchit@gmail.com>
2024-12-12 20:23:05 +0530

    Implemented Lazy loading

--- src/components/PhotoCard.js
+++ src/components/PhotoCard.js
@@ -1,20 +1,23 @@
 import React, { useState } from "react";
 import styles from "./PhotoCard.module.css";
+import LazyLoad from "react-lazyload";
 
 function PhotoCard({ src, name }) {
   const [likes, setLikes] = useState(0); // State to track likes
 
   return (
-    <div className={styles.card} role="img" aria-label={'Photo by ${name}'}>
-      <img src={src} alt={`Phot by ${name}`} className={styles.image} />
-      <p>{name}</p>
-      <button
-        onClick={() => setLikes(likes + 1)}
-        className={styles.button} 
-      >
-        ❤️ {likes} Likes
-      </button>
-    </div>
+    <LazyLoad height={200} offset={100}>
+      <div className={styles.card} role="img" aria-label={'Photo by ${name}'}>
+        <img src={src} alt={`Phot by ${name}`} className={styles.image} />
+        <p>{name}</p>
+        <button
+          onClick={() => setLikes(likes + 1)}
+          className={styles.button} 
+        >
+          ❤️ {likes} Likes
+        </button>
+      </div>
+    </LazyLoad>
   );
 }
 
