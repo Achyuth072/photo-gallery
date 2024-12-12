25591db69083a23f24fe0ed9c46111a5f490fda1
Achyuth Suchit <achyuthsuchit@gmail.com>
2024-12-12 18:42:26 +0530

    Added added accessibility features by adding alt tags and Aria roles

--- src/components/PhotoCard.js
+++ src/components/PhotoCard.js
@@ -5,8 +5,8 @@ function PhotoCard({ src, name }) {
   const [likes, setLikes] = useState(0); // State to track likes
 
   return (
-    <div className={styles.card}>
-      <img src={src} alt={`${name}'s photo`} className={styles.image} />
+    <div className={styles.card} role="img" aria-label={'Photo by ${name}'}>
+      <img src={src} alt={`Phot by ${name}`} className={styles.image} />
       <p>{name}</p>
       <button
         onClick={() => setLikes(likes + 1)}
